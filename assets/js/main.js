5a922c16b183a5a0cef959c46bd4370c6c0d1f8f
israaqun <israaali848@gmail.com>
2023-09-23 14:23:38 +0300

    Revert Gulp changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,211 @@
-function deleteAllTasks(){taskList.innerHTML="",existingTasks.clear(),saveTaskListToLocalStorage(),updateDeleteAllButtonVisibility()}document.addEventListener("DOMContentLoaded",function(){const d=document.getElementById("taskList");var t=document.getElementById("listunav");const c=new Set;function i(){var t=document.getElementById("deleteAllButton");0<d.children.length?t.style.display="block":t.style.display="none"}function r(){var t=d.innerHTML;localStorage.setItem("taskList",t)}!function(){try{return localStorage.setItem("test","test"),localStorage.removeItem("test"),1}catch(t){}}()?(d.style.display="none",t.style.display="block"):(d.style.display="block",t.style.display="none",(t=localStorage.getItem("taskList"))&&(d.innerHTML=t,d.querySelectorAll(".task-text").forEach(function(t){c.add(t.textContent)})),i()),i(),d.addEventListener("click",function(t){if((t=t.target).classList.contains("delete-button")){var e=t.closest(".task-item"),n=e.querySelector(".task-text");n&&(n=n.textContent,e.remove(),c.delete(n),i(),r())}else if(t.classList.contains("update-button")){const a=t.closest(".task-item"),s=a.querySelector(".task-text");if(s){const l=s.textContent,o=document.createElement("input"),d=(o.classList.add("edit-input"),o.value=l,document.createElement("button"));d.classList.add("save-button"),d.textContent="Save",d.addEventListener("click",function(){var t,e=o.value;l!==(t=e)&&c.has(t)?alert("Task already exists. Please enter a different task."):(s.textContent=e,(t=document.createElement("button")).classList.add("update-button"),t.textContent="Update",a.querySelector(".task-actions").appendChild(t),a.removeChild(o),a.removeChild(d),r(),i())}),a.querySelector(".task-actions").removeChild(t),a.appendChild(o),a.appendChild(d),o.focus()}}}),document.querySelector(".task-button").addEventListener("click",function(){var t=document.getElementById("taskInput");const e=t.value.trim();if(""===e)alert("Please enter a task.");else{var n=e.toLowerCase().replace(/ /g,"-").replace(/[^a-zA-Z0-9-]/g,"");if(console.log("Task Slug:",n),n=e,c.has(n))alert("Task already exists. Please enter a different task.");else{const o=document.createElement("li");o.classList.add("task-item");(n=document.createElement("span")).classList.add("task-text"),n.textContent=e;var a=document.createElement("div"),s=(a.classList.add("task-actions"),document.createElement("button")),l=(s.classList.add("update-button"),s.textContent="Update",document.createElement("button"));l.classList.add("delete-button"),l.textContent="Delete",l.addEventListener("click",function(){o.remove(),c.delete(e),r(),i()}),a.appendChild(s),a.appendChild(l),o.appendChild(n),o.appendChild(a),d.appendChild(o),t.value="",c.add(e),r(),i()}}})});
+document.addEventListener("DOMContentLoaded", function () {
+    const taskList = document.getElementById("taskList");
+    const listunav = document.getElementById("listunav");
+    const existingTasks = new Set();
+
+    if (isLocalStorageAvailable()) {
+        // If local storage is available, display the task list and hide the "No tasks" message.
+        taskList.style.display = "block";
+        listunav.style.display = "none";
+
+        // Load tasks from local storage and populate the task list.
+        loadTasksFromLocalStorage();
+        updateDeleteAllButtonVisibility();
+    } else {
+        // If local storage is not available, display the "No tasks" message and hide the task list.
+        taskList.style.display = "none";
+        listunav.style.display = "block";
+    }
+
+    function isTaskUnique(taskText) {
+        return !existingTasks.has(taskText);
+    }
+
+    function isTaskUpdatedTextUnique(taskText, updatedText) {
+        // Check if the updated task text is unique among existing tasks
+        if (taskText === updatedText) {
+            return true; // No change in text, consider it unique
+        }
+        return !existingTasks.has(updatedText);
+    }
+
+    // Function to update the visibility of the "Delete All" button
+    function updateDeleteAllButtonVisibility() {
+        const deleteAllButton = document.getElementById("deleteAllButton");
+
+        if (taskList.children.length > 0) {
+            deleteAllButton.style.display = "block"; // Show the button
+        } else {
+            deleteAllButton.style.display = "none"; // Hide the button
+        }
+    }
+
+    // Call the function initially to set the initial visibility
+    updateDeleteAllButtonVisibility();
+
+    function loadTasksFromLocalStorage() {
+        const savedTasks = localStorage.getItem("taskList");
+
+        if (savedTasks) {
+            // If there are saved tasks in local storage, populate the task list.
+            taskList.innerHTML = savedTasks;
+            // Populate the set of existing tasks
+            taskList.querySelectorAll(".task-text").forEach(function (taskTextElement) {
+                existingTasks.add(taskTextElement.textContent);
+            });
+        }
+    }
+
+    function saveTaskListToLocalStorage() {
+        const tasks = taskList.innerHTML;
+        localStorage.setItem("taskList", tasks);
+    }
+
+    function isLocalStorageAvailable() {
+        try {
+            localStorage.setItem("test", "test");
+            localStorage.removeItem("test");
+            return true;
+        } catch (e) {
+            return false;
+        }
+    }
+
+    function createTask() {
+        const taskInput = document.getElementById("taskInput");
+        const taskText = taskInput.value.trim();
+
+        if (taskText === "") {
+            alert("Please enter a task.");
+            return;
+        }
+        function slugify(text) {
+            return text
+                .toLowerCase()
+                .replace(/ /g, "-") // Replace spaces with hyphens
+                .replace(/[^a-zA-Z0-9-]/g, ""); // Remove special characters
+        }
+        const slug = slugify(taskText);
+        console.log("Task Slug:", slug);
+
+        if (!isTaskUnique(taskText)) {
+            alert("Task already exists. Please enter a different task.");
+            return;
+        }
+
+        const listItem = document.createElement("li");
+        listItem.classList.add("task-item");
+
+        const taskTextElement = document.createElement("span");
+        taskTextElement.classList.add("task-text");
+        taskTextElement.textContent = taskText;
+
+        const taskActions = document.createElement("div");
+        taskActions.classList.add("task-actions");
+
+        const updateButton = document.createElement("button");
+        updateButton.classList.add("update-button");
+        updateButton.textContent = "Update";
+
+        const deleteButton = document.createElement("button");
+        deleteButton.classList.add("delete-button");
+        deleteButton.textContent = "Delete";
+        deleteButton.addEventListener("click", function () {
+            listItem.remove();
+            existingTasks.delete(taskText);
+            saveTaskListToLocalStorage();
+            updateDeleteAllButtonVisibility();
+        });
+
+        taskActions.appendChild(updateButton);
+        taskActions.appendChild(deleteButton);
+
+        listItem.appendChild(taskTextElement);
+        listItem.appendChild(taskActions);
+
+        taskList.appendChild(listItem);
+
+        taskInput.value = "";
+        existingTasks.add(taskText); // Add the task to the set of existing tasks
+        saveTaskListToLocalStorage();
+        updateDeleteAllButtonVisibility();
+    }
+
+    taskList.addEventListener("click", function (event) {
+        const target = event.target;
+
+        if (target.classList.contains("delete-button")) {
+            const listItem = target.closest(".task-item");
+            const taskTextElement = listItem.querySelector(".task-text");
+
+            if (taskTextElement) {
+                const taskText = taskTextElement.textContent;
+                listItem.remove();
+                existingTasks.delete(taskText);
+                updateDeleteAllButtonVisibility();
+                saveTaskListToLocalStorage();
+            }
+        } else if (target.classList.contains("update-button")) {
+            const listItem = target.closest(".task-item");
+            const taskTextElement = listItem.querySelector(".task-text");
+
+            if (taskTextElement) {
+                const currentTaskText = taskTextElement.textContent;
+                const taskInput = document.createElement("input");
+                taskInput.classList.add("edit-input");
+                taskInput.value = currentTaskText;
+
+                const saveButton = document.createElement("button");
+                saveButton.classList.add("save-button");
+                saveButton.textContent = "Save";
+
+                saveButton.addEventListener("click", function () {
+                    const editedTaskText = taskInput.value;
+                    if (!isTaskUpdatedTextUnique(currentTaskText, editedTaskText)) {
+                        alert("Task already exists. Please enter a different task.");
+                        return;
+                    }
+                    taskTextElement.textContent = editedTaskText;
+
+                    const updateButton = document.createElement("button");
+                    updateButton.classList.add("update-button");
+                    updateButton.textContent = "Update";
+
+                    listItem.querySelector(".task-actions").appendChild(updateButton);
+
+                    listItem.removeChild(taskInput);
+                    listItem.removeChild(saveButton);
+
+                    saveTaskListToLocalStorage();
+                    updateDeleteAllButtonVisibility();
+                });
+
+                listItem.querySelector(".task-actions").removeChild(target);
+
+                listItem.appendChild(taskInput);
+                listItem.appendChild(saveButton);
+
+                taskInput.focus();
+            }
+        }
+    });
+
+    function deleteAllTasks() {
+        taskList.innerHTML = ""; // Clear all tasks from the list
+        existingTasks.clear(); // Clear the set of existing tasks
+        saveTaskListToLocalStorage(); // Update local storage
+        updateDeleteAllButtonVisibility();
+    }
+    
+
+    // Find the "Create" button by its class name and add an event listener
+    const createButton = document.querySelector(".task-button");
+    createButton.addEventListener("click", createTask);
+});
+function deleteAllTasks() {
+
+    taskList.innerHTML = ""; // Clear all tasks from the list
+    existingTasks.clear(); // Clear the set of existing tasks
+    saveTaskListToLocalStorage(); // Update local storage
+    updateDeleteAllButtonVisibility();
+}
